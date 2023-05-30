bc041ae9f8b047e853e817646bf10a82a71ed251
rachel-oromo <124389469+rachel-oromo@users.noreply.github.com>
2023-05-30 01:28:04 +0000

    sync changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Rachel Oromo
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check service worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit3-03/sw.js", {
+    scope: "/ICS2O-Unit3-03/",
+  })
+}
+
+/**
+ * This function calculates the volume of a pyramid
+ */
+function enterClicked() {
+  //input
+  const radius = parseFloat(document.getElementById("radius-of-sphere").value)
+
+  //process
+  const volume = (4.0 * Math.PI * radius ** 3) / 3.0
+
+  //output
+  document.getElementById("volume-of-sphere").innerHTML =
+    "The volume of the sphere is: " + volume.toFixed(2) + " cm³."
+}
